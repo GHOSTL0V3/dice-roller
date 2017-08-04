e02e2fdee8ccabbe65875f30b9fc4de8013e7215
Jesse Laflair <laflair.j@gmail.com>
2017-08-03 17:10:39 -0700

    Add existing file

---
+++ Character-Builder.py
@@ -0,0 +1,6 @@
+name = raw_input("What is your name?")
+character_class = raw_input("What is your class?")
+race = raw_input("What is your race?")
+
+print ("Ah, so your name is %s, your class is %s, " \
+"and your race is %s.") % (name, character_class, race)
